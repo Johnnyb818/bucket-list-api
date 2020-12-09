8f893730b2a8026e9686ddaad5f03fac9580402b
John Bruno <johnnyb@MacBook-Pro.local>
2020-12-08 19:32:43 -0600

    Buckets API setup and working

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_210737) do
+ActiveRecord::Schema.define(version: 2020_12_08_235816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buckets", force: :cascade do |t|
+    t.string "title"
+    t.string "location"
+    t.text "description"
+    t.string "image"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_buckets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_12_08_210737) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "buckets", "users"
 end
